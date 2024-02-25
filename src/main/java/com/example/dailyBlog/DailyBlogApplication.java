f482563bbc1c077b93793fcbdecb02b86d16ceb4
vanboot1999 <yifanwu810@gmail.com>
2024-02-25 03:27:35 +0000

    first commit

---
+++ src/main/java/com/example/dailyBlog/DailyBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.dailyBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dailyBlog/DailyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dailyBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
